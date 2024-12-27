b7e4dec95292ce28bcaa2a2eebef714897206483
Simen A. W. Olsen <hello@simenandre.no>
2024-12-27 15:35:07 +0100

    add blink.cmp to deps

--- lua/simenandre/plugins/lsp.lua
+++ lua/simenandre/plugins/lsp.lua
@@ -24,7 +24,7 @@ return {
         { "j-hui/fidget.nvim", opts = {} },
 
         -- Allows extra capabilities provided by nvim-cmp
-        "hrsh7th/cmp-nvim-lsp",
+        "saghen/blink.cmp",
       },
       config = function()
         -- Brief aside: **What is LSP?**
