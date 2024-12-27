62df997a42e292c22ee4ce6a5414735cd2e37699
Simen A. W. Olsen <hello@simenandre.no>
2024-12-27 15:33:47 +0100

    split plugins into each file

---
+++ lua/simenandre/plugins/misc.lua
@@ -0,0 +1,18 @@
+return {
+  "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    },
+  },
+}
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
